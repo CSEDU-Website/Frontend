e8634c355549a301fd0a21eb20750f56da418335
Waki Assami <waki@Wakis-MacBook-Air.local>
2025-07-13 16:30:37 +0600

    admission form connected to frontend,navigation fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  server: {
+    proxy: {
+      '/utility': 'http://localhost:8000',
+      '/guest': 'http://localhost:8000',
+      '/v1': 'http://localhost:8000',
+    }
+  }
 })
